ea86c12cf642af30bec5cb59a59fc9160b3c689d
CircleCube89 <IvGri2@yandex.ru>
2022-06-03 11:56:51 +0300

    2 practika

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from .models import Comment
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status']
@@ -11,3 +12,8 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ['status', 'publish']
 
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
